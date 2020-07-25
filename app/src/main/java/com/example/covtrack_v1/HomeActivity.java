2caa7842c3c120ecdfbff7bd55d10fd239f69545
dinudante <dineshdante.ds@gmail.com>
2020-07-25 18:30:59 +0530

    Jul 25 6:30PM

--- app/src/main/java/com/example/covtrack_v1/HomeActivity.java
+++ app/src/main/java/com/example/covtrack_v1/HomeActivity.java
@@ -1,21 +1,73 @@
 package com.example.covtrack_v1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button btn;
+    EditText numTxt;
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         BottomNavigationView bottomNavigationView =findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item1:
+                        Toast.makeText(HomeActivity.this, "Item1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item2:
+                        Toast.makeText(HomeActivity.this, "Item2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.item3:
+                        Toast.makeText(HomeActivity.this, "Item3", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(HomeActivity.this, ScrollingActivity1.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.item4:
+                        Toast.makeText(HomeActivity.this, "Item4", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        switch (item.getItemId()) {
+            case R.id.mitem1:
+                Toast.makeText(this, "Item1", Toast.LENGTH_SHORT).show();
+            case R.id.mitem2:
+                Toast.makeText(this, "Item2", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
