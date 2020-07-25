5ba10a6917727881560bf6e9a40e96de8ac339c0
dinudante <dineshdante.ds@gmail.com>
2020-07-25 07:53:27 +0530

    Jul 25 6:17AM

--- app/src/main/java/com/example/covtrack_v1/HomeActivity.java
+++ app/src/main/java/com/example/covtrack_v1/HomeActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNavigationView =findViewById(R.id.navigation);
     }
 
-    
+
 
 }
